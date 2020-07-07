37349f2d491268b91993c04987cf52f57dbde963
Lszh0147 <no@email.com>
2020-07-07 16:19:55 +0800

    删除 com.just.agentweb

--- app/src/main/java/com/sollyu/android/appenv/activitys/ActivityDetail.kt
+++ app/src/main/java/com/sollyu/android/appenv/activitys/ActivityDetail.kt
@@ -29,7 +29,7 @@ import com.alibaba.fastjson.JSON
 import com.alibaba.fastjson.JSONObject
 //import com.elvishew.xlog.XLog
 import com.github.rubensousa.bottomsheetbuilder.BottomSheetBuilder
-import com.just.agentweb.AgentWebConfig
+//import com.just.agentweb.AgentWebConfig
 import com.sollyu.android.appenv.R
 import com.sollyu.android.appenv.bean.BeanHookInfo
 import com.sollyu.android.appenv.bean.PhoneModel
